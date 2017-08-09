bb6df66ff783707192dd0839b186247a0c076270
Paulo <Paulo@Asus>
2017-08-08 23:35:45 -0300

    Constantes

---
+++ app/scripts/app.constant.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('mvsApp')
+        .constant('Constante', {
+            BASE_URL: 'http://148.240.92.98:8484',
+            CAM_URL: 'http://localhost'
+        });
+})();
