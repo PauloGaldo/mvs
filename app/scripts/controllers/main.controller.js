d3dd299f172f6aa88de94abc080a0bd99a146523
Paulo <Paulo@Asus>
2017-08-08 23:35:20 -0300

    Funciones para mover camara migradas a angular

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -5,12 +5,81 @@
             .module('mvsApp')
             .controller('MainCtrl', MainCtrl);
 
-    MainCtrl.$inject = ['$stateParams'];
+    MainCtrl.$inject = ['$stateParams', 'Constante', 'ComunicationService'];
 
-    function MainCtrl($stateParams) {
+    function MainCtrl($stateParams, Constante, ComunicationService) {
         var vm = this;
+        vm.moveRight = moveRight;
+        vm.moveLeft = moveLeft;
+        vm.moveUp = moveUp;
+        vm.moveDown = moveDown;
+        vm.stop = stop;
+        vm.zoomOut = zoomOut;
+        vm.zoomIn = zoomIn;
 
-        $f().play([{url: 'http://108.59.81.161/video/flash/' + $stateParams.id}]);
+
+        /**
+         * Movimiento a la derecha
+         * @returns {undefined}
+         */
+        function moveRight() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/move?action=right";
+            ComunicationService.post(url);
+        }
+
+        /**
+         * Movimiento a la izquierda
+         * @returns {undefined}
+         */
+        function moveLeft() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/move?action=left";
+            ComunicationService.post(url);
+        }
+
+        /**
+         * Movminiento hacia arriba
+         * @returns {undefined}
+         */
+        function moveUp() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/move?action=up";
+            ComunicationService.post(url);
+        }
+
+        /**
+         * Movimiento hacia abajo
+         * @returns {undefined}
+         */
+        function moveDown() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/move?action=down";
+            ComunicationService.post(url);
+        }
+
+        /**
+         * Detener movimiento
+         * @returns {undefined}
+         */
+        function stop() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/move?action=stop";
+            ComunicationService.post(url);
+        }
+
+        /**
+         * Alejar camara
+         * @returns {undefined}
+         */
+        function zoomOut() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/zoom?action=out";
+            ComunicationService.post(url);
+        }
+
+        /**
+         * Acercar camara
+         * @returns {undefined}
+         */
+        function zoomIn() {
+            var url = Constante.BASE_URL + "/api/v1/camera/" + $stateParams.id + "/zoom?action=in";
+            ComunicationService.post(url);
+        }
     }
 
 })();
