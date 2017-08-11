a4994c17422cfd4821e6943b3c42c755ae7f257d
Paulo <Paulo@Asus>
2017-08-11 02:56:41 -0300

    Url modificada para obtener video en html5

--- app/scripts/controllers/native.controller.js
+++ app/scripts/controllers/native.controller.js
@@ -10,7 +10,7 @@
     function NativeCtrlr($stateParams, Constante) {
         var vm = this;
         vm.id = $stateParams.id;
-        vm.source = Constante.CAM_URL + '/html5/' + $stateParams.id;
+        vm.source = Constante.CAM_URL + '/video/html5/' + $stateParams.id;
     }
 
 })();
