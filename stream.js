f1598394cf9b1bd672d334bb0a2991c551109cae
Paulo <Paulo@Asus>
2017-08-11 01:27:34 -0300

    Grunt reemplazado

--- stream.js
+++ stream.js
@@ -1,69 +1,23 @@
-const express = require('express')();
-const http = require('http');
+const express = require('express');
+const app = express();
 const ffmpeg = require('fluent-ffmpeg');
-const server = require('http').Server(express);
-const path = require('path');
+const http = require('http').createServer(app);
+const sender = require('http');
+const io = require('socket.io')(http);
+const Log = require('log');
+let LOGGER = new Log('debug');
+let port = process.env.PORT || 8080;
 
-function mapperCamera(guid, callback) {
-    var id;
-    switch (guid) {
-        case "00000000-0000-0000-0000-00047e02545b":
-            id = 10;
-            break;
-        case "00000000-0000-0000-0000-00047e025587":
-            id = 14;
-            break;
-        case "00000000-0000-0000-0000-00047e025a48":
-            id = 15;
-            break;
-        default:
-            id = 1;
-            break;
-    }
-    var path = '/api/device-manager/cameras/' + id + '/stream-url';
-    var options = {
-        hostname: '148.240.92.98',
-        port: 8484,
-        path: path,
-        method: 'GET',
-        auth: 'admin:admin',
-        headers: {
-            'Content-Type': 'text/plain;charset=UTF-8'
-        }
-    };
-    http.request(options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log(chunk);
-            return callback(chunk);
-        });
-    }).end();
-}
+app.listen(port, function () {
+    LOGGER.info('Listening port: ' + port);
+});
 
-function setCameraUrl(guid, callback) {
-    var path = '/api/v1/camera/' + guid + '/video/url';
-    var options = {
-        hostname: '148.240.92.98',
-        port: 8484,
-        path: path,
-        method: 'GET',
-        auth: 'admin:admin',
-        headers: {
-            'Content-Type': 'text/plain;charset=UTF-8'
-        }
-    };
-    http.request(options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log(chunk);
-            return callback(chunk);
-        });
-    }).end();
-}
+app.use(express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+//app.use('/socket.io', express.static(__dirname + '/socket.io'));
 
-
-server.listen(80, function () {
-    console.log('Listening on localhost:8080');
+io.on('connection', function (socket) {
+    LOGGER.info('connected to socket', socket);
 });
 
 /**
@@ -73,7 +27,7 @@ server.listen(80, function () {
  * @param {type} req 
  * @param {type} res 
  */
-express.get('/video/flash/:id', function (req, res) {
+app.get('/video/flash/:id', function (req, res) {
     console.log("id: " + req.params.id);
     setCameraUrl(req.params.id, function (url) {
         res.contentType('flv');
@@ -103,7 +57,7 @@ express.get('/video/flash/:id', function (req, res) {
  * @param {type} req 
  * @param {type} res 
  */
-express.get('/html5/:id', function (req, res) {
+app.get('/html5/:id', function (req, res) {
     console.log("id: " + req.params.id);
     setCameraUrl(req.params.id, function (url) {
         ffmpeg(url)
@@ -126,37 +80,59 @@ express.get('/html5/:id', function (req, res) {
     });
 });
 
+function setCameraUrl(guid, callback) {
+    var path = '/api/v1/camera/' + guid + '/video/url';
+    var options = {
+        hostname: '148.240.92.98',
+        port: 8484,
+        path: path,
+        method: 'GET',
+        auth: 'admin:admin',
+        headers: {
+            'Content-Type': 'text/plain;charset=UTF-8'
+        }
+    };
+    sender.request(options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log(chunk);
+            return callback(chunk);
+        });
+    }).end();
+}
 
-express.get('/', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
-});
-
-express.get('/flowplayer/flowplayer.controls.swf', function (req, res) {
-    res.sendFile(__dirname + '/dist/flowplayer/flowplayer.controls.swf');
-});
-
-express.get('/flowplayer/flowplayer.min.js', function (req, res) {
-    res.sendFile(__dirname + '/dist/flowplayer/flowplayer.min.js');
-});
-
-express.get('/flowplayer/flowplayer.swf', function (req, res) {
-    res.sendFile(__dirname + '/dist/flowplayer/flowplayer.swf');
-});
-
-/*ESTAS DIRECCIONES DEBEN ACTUALIZARCE CADA QUE SE HAGA UN BUILD*/
-
-express.get('/scripts/scripts.2aba9a6c.js', function (req, res) {
-    res.sendFile(__dirname + '/dist/scripts/scripts.2aba9a6c.js');
-});
-
-express.get('/scripts/vendor.3014206a.js', function (req, res) {
-    res.sendFile(__dirname + '/dist/scripts/vendor.3014206a.js');
-});
-
-express.get('/styles/main.6230cc0e.css', function (req, res) {
-    res.sendFile(__dirname + '/dist/styles/main.6230cc0e.css');
-});
-
-express.get('/styles/vendor.d41d8cd9.css', function (req, res) {
-    res.sendFile(__dirname + '/dist/styles/vendor.d41d8cd9.css');
-});
+function mapperCamera(guid, callback) {
+    var id;
+    switch (guid) {
+        case "00000000-0000-0000-0000-00047e02545b":
+            id = 10;
+            break;
+        case "00000000-0000-0000-0000-00047e025587":
+            id = 14;
+            break;
+        case "00000000-0000-0000-0000-00047e025a48":
+            id = 15;
+            break;
+        default:
+            id = 1;
+            break;
+    }
+    var path = '/api/device-manager/cameras/' + id + '/stream-url';
+    var options = {
+        hostname: '148.240.92.98',
+        port: 8484,
+        path: path,
+        method: 'GET',
+        auth: 'admin:admin',
+        headers: {
+            'Content-Type': 'text/plain;charset=UTF-8'
+        }
+    };
+    sender.request(options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log(chunk);
+            return callback(chunk);
+        });
+    }).end();
+}
