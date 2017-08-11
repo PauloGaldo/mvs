db8a8260a2c2b09341cb3083d5fa78f1b76af27f
Paulo <Paulo@Asus>
2017-08-11 02:54:58 -0300

    Socket controller

---
+++ app/scripts/controllers/socket.controller.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+
+    angular
+            .module('mvsApp')
+            .controller('SocketCtrlr', SocketCtrlr);
+
+    SocketCtrlr.$inject = ['$stateParams'];
+
+    function SocketCtrlr($stateParams) {
+        var vm = this;
+
+        var socket = io();
+        var canvas = document.getElementById("preview");
+        var context = canvas.getContext("2d");
+        canvas.width = 800;
+        canvas.height = 600;
+        context.width = canvas.width;
+        context.height = canvas.height;
+        var video = document.getElementById("video");
+
+        socket.on('start', function (response) {
+            console.log(response);
+            socket.emit('streaming', $stateParams.id);
+            var cont = 0;
+            socket.on('data', function (response) {
+                console.log(response, cont++);
+                var bytes = new Uint8Array(response);
+                var blob = new Blob([bytes], {type: 'application/octet-binary'});
+                var url = URL.createObjectURL(blob);
+                var img = new Image;
+                img.onload = function () {
+                    URL.revokeObjectURL(url);
+                    context.drawImage(img, 0, 0, context.width, context.height);
+                };
+                img.src = url;
+            });
+        });
+
+
+
+    }
+
+})();
