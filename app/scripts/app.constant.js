e85d376793c737e7c7b6a23626d3a7284e3ae993
Paulo Galdo <e-pgaldo@neoris.com>
2017-08-15 15:59:18 -0300

    JWT agregado

--- app/scripts/app.constant.js
+++ app/scripts/app.constant.js
@@ -4,7 +4,8 @@
     angular
         .module('mvsApp')
         .constant('Constante', {
-            BASE_URL: 'http://148.240.92.98:8484',
+            BASE_URL: 'http://192.168.80.173:8080',
+            JWT: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfTFBSLFJPTEVfUkZJRCxST0xFX1VTRVIsUk9MRV9XIiwiZXhwIjoxNTExNDU3NzI0fQ.-ClxLK4sKJD3iQrFpxvJzrW6TvGzRONCwjhrAOJPN2QOFjxcGJ8ndW9anfMcFHL18DBtrlCtIM9mkloKT-RhNg',
             CAM_URL: 'http://104.198.36.174'
         });
 })();
