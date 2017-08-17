d788011015565b03f524c9d7a1b05a941c9864c6
Paulo <Paulo@Asus>
2017-08-17 00:38:32 -0300

    Constantes dinamicas

--- app/scripts/app.constant.js
+++ app/scripts/app.constant.js
@@ -1,11 +1,13 @@
 (function () {
     'use strict';
-
-    angular
-        .module('mvsApp')
-        .constant('Constante', {
-            BASE_URL: 'http://192.168.80.173:8080',
-            JWT: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfTFBSLFJPTEVfUkZJRCxST0xFX1VTRVIsUk9MRV9XIiwiZXhwIjoxNTExNDU3NzI0fQ.-ClxLK4sKJD3iQrFpxvJzrW6TvGzRONCwjhrAOJPN2QOFjxcGJ8ndW9anfMcFHL18DBtrlCtIM9mkloKT-RhNg',
-            CAM_URL: 'http://104.198.36.174'
-        });
+    $.getJSON("config.json", function (json) {
+        console.log(json);
+        angular
+                .module('mvsApp')
+                .constant('Constante', {
+                    BASE_URL: 'http://' + json.host_modulo + ':' + json.port_modulo,
+                    JWT: json.token,
+                    CAM_URL: 'http://' + json.host_stream + ':' + json.port_stream
+                });
+    });
 })();
